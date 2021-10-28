61db3f000498e9ec3d8303b1b9e1f4df9859670a
Aramis1303 <aramis1303@gmail.com>
2021-10-28 12:33:20 +0300

    created registeration and login temlates

--- src/main/java/ru/evaproj/analyst/controllers/LoginController.java
+++ src/main/java/ru/evaproj/analyst/controllers/LoginController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import ru.evaproj.analyst.dto.UserDTO;
 import ru.evaproj.analyst.services.UserService;
@@ -18,7 +19,8 @@ public class LoginController {
     UserService userService;
 
     @GetMapping(value = "/login")
-    public String getLoginPage() {
+    public String getLoginPage(Model model) {
+        model.addAttribute("user", new UserDTO());
         return "login";
     }
 
