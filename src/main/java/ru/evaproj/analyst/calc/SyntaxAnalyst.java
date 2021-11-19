48979ea902ab35073d77ff2feaa7b353d0d29dca
Aramis1303 <aramis1303@gmail.com>
2021-11-20 01:13:49 +0300

    Added syntax parser. Added html resource access.

--- src/main/java/ru/evaproj/analyst/calc/SyntaxAnalyst.java
+++ src/main/java/ru/evaproj/analyst/calc/SyntaxAnalyst.java
@@ -1,4 +1,48 @@
 package ru.evaproj.analyst.calc;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Field;
+import java.util.*;
+
+@Slf4j
 public class SyntaxAnalyst {
+
+
+    /*  Функция принимает список объектов и наименование строкового поля для
+        дальнейшего синтаксического анализа.
+        Функция разбирает стоку на слова и возвращает список сключенных слов
+        с ссылками на объекты в которых данная строка присутствует.
+    */
+    public Map<String, Set<Object>> parsing (List<? extends Object> list, String fieldName) throws NoSuchFieldException {
+
+        if (list.size() == 0)
+            throw new RuntimeException ("Size of your data is 0.");
+
+        Map <String, Set <Object>> links = new HashMap<>();
+        Field field = list.get(0).getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+
+        list.
+                stream().
+                forEach((obj) -> {
+                    try {
+                        Arrays.asList(((String)field.get(obj)).split(" "))
+                                .stream()
+                                .forEach(s -> {
+                                    if (links.containsKey(s)) {
+                                        links.get(s).add(obj);
+                                    }
+                                    else {
+                                        links.put(s, new HashSet<>());
+                                        links.get(s).add(obj);
+                                    }
+                                });
+                    } catch (IllegalArgumentException | IllegalAccessException ex) {
+                        log.error(ex.getMessage());
+                    }
+                });
+
+        return links;
+    }
 }
--- src/main/java/ru/evaproj/analyst/configs/WebSecurityConfig.java
+++ src/main/java/ru/evaproj/analyst/configs/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -58,5 +59,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(bCryptPasswordEncoder());
     }
 
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/templates/static/**");
+    }
 
 }
